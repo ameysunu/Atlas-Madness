6ca3d87a4e1fc6a0b16a8174f72b01ced30f3055
ameysunu <ameysunu00719@gmail.com>
2023-06-21 20:44:28 +0100

    iOS app added

---
+++ AtlasGoogleCloud/AtlasGoogleCloud/AtlasGoogleCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasGoogleCloudApp.swift
+//  AtlasGoogleCloud
+//
+//  Created by Amey Sunu on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AtlasGoogleCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
