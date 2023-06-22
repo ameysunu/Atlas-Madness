5ddb4ff95b81dc258de42f78a9c5ce7632cef494
ameysunu <ameysunu00719@gmail.com>
2023-06-22 18:32:00 +0100

    build: textfield added and tied to the function

--- AtlasGoogleCloud/AtlasGoogleCloud/Views/WelcomeView.swift
+++ AtlasGoogleCloud/AtlasGoogleCloud/Views/WelcomeView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 struct WelcomeView: View {
     let hour = Calendar.current.component(.hour, from: Date())
     func determineGreeting(hour: Int) -> String {
-            switch hour {
-            case 0..<12:
-                return "Morning"
-            case 12..<17:
-                return "Afternoon"
-            default:
-                return "Evening"
-            }
+        switch hour {
+        case 0..<12:
+            return "Morning"
+        case 12..<17:
+            return "Afternoon"
+        default:
+            return "Evening"
         }
+    }
+    @State private var userId: String = ""
+    @State private var password: String = ""
     var body: some View {
         VStack{
             HStack{
@@ -32,8 +34,21 @@ struct WelcomeView: View {
                 Spacer()
             }
             Spacer()
+            TextField("User ID",text: $userId)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 5.0)
+                    .stroke(lineWidth: 1.0))
+                .padding(.bottom, 10)
+                .font(.custom("EBGaramond-Regular", size: 20))
+            
+            SecureField("Password",text: $password)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 5.0)
+                    .stroke(lineWidth: 1.0))
+                .padding(.bottom, 10)
+                .font(.custom("EBGaramond-Regular", size: 20))
             Button(action:{
-                loginUser(userId: "dfdfdf", password: "testData")
+                loginUser(userId: userId, password: password)
             }){
                 Text("Login")
                     .font(.custom("EBGaramond-Regular", size: 25))
@@ -45,7 +60,7 @@ struct WelcomeView: View {
                         .stroke(lineWidth: 0.1)
                         .shadow(color: .black, radius: 10.0))
                     .background(RoundedRectangle(cornerRadius: 10).fill(.black))
-                .padding(.top, 20)
+                    .padding(.top, 20)
                 
             }
             Button(action:{}){
