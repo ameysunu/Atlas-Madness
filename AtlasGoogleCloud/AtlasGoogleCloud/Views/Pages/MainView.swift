a5c2ba1ee6437797f96d6b8caf8670621539ad6d
ameysunu <ameysunu00719@gmail.com>
2023-06-25 14:32:34 +0100

    build: get average mood built in

--- AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/MainView.swift
+++ AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/MainView.swift
@@ -12,6 +12,9 @@ struct MainView: View {
     @State var avgEnergyLevel: Double = 0.0
     @State var avgSleepLevel: Double = 0.0
     @State var averageRating: Double = 0.0
+    @State var moodRange: String = ""
+    @State var isLoading: Bool = true
+    @State var isTimeout: Bool = false
     
     var body: some View {
         VStack{
@@ -20,99 +23,124 @@ struct MainView: View {
                     .font(.custom("EBGaramond-Regular", size: 30))
                 Spacer()
             }
-            Rectangle()
-                .cornerRadius(5)
-                .offset(x: 4, y: 4)
-                .frame(height: 100)
-                .overlay(
+            if isLoading {
+                if isTimeout{
+                    Text("There is no enough data to create your dashboard or there is an issue with internet connectivity.")
+                        .font(.custom("EBGaramond-Regular", size: 20))
+                } else {
+                    HStack{
+                        Text("Loading your personalized dashboard")
+                            .font(.custom("EBGaramond-Regular", size: 20))
+                        ProgressView()
+                            .padding(.leading, 5)
+                    }
+                }
+            } else {
+                Rectangle()
+                    .cornerRadius(5)
+                    .offset(x: 4, y: 4)
+                    .frame(height: 100)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.black, lineWidth: 5)
+                            .background(.white)
+                            .cornerRadius(5)
+                            .overlay(
+                                HStack{
+                                    VStack(alignment: .leading){
+                                        Text("Your general mood has been:")
+                                            .font(.custom("EBGaramond-Regular", size: 15))
+                                            .padding(.top, 5)
+                                        Text("\(moodRange)!")
+                                            .font(.custom("EBGaramond-Regular", size: 30))
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                                    .padding()
+                            )
+                    )
+                    .padding(.top, 5)
+                HStack{
                     Rectangle()
-                        .stroke(.black, lineWidth: 5)
-                        .background(.white)
                         .cornerRadius(5)
+                        .offset(x: 4, y: 4)
+                        .frame(height: 100)
                         .overlay(
-                            HStack{
-                                VStack(alignment: .leading){
-                                    Text("Your general mood has been:")
-                                        .font(.custom("EBGaramond-Regular", size: 15))
-                                        .padding(.top, 5)
-                                    Text("Happy!")
-                                        .font(.custom("EBGaramond-Regular", size: 30))
-                                    Spacer()
-                                }
-                                Spacer()
-                            }
-                                .padding()
+                            Rectangle()
+                                .stroke(.black, lineWidth: 5)
+                                .background(returnBoxColor(params: avgEnergyLevel))
+                                .cornerRadius(5)
+                                .overlay(
+                                    HStack{
+                                        VStack(alignment: .leading){
+                                            Text("Average Energy Level")
+                                                .font(.custom("EBGaramond-Regular", size: 15))
+                                                .padding(.top, 5)
+                                            Text("\(avgEnergyLevel, specifier: "%.1f")")
+                                                .font(.custom("EBGaramond-Regular", size: 30))
+                                            Spacer()
+                                        }
+                                        Spacer()
+                                    }
+                                        .padding()
+                                    
+                                    
+                                    
+                                )
                         )
-                )
-                .padding(.top, 5)
-            HStack{
-                Rectangle()
-                    .cornerRadius(5)
-                    .offset(x: 4, y: 4)
-                    .frame(height: 100)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.black, lineWidth: 5)
-                            .background(returnBoxColor(params: avgEnergyLevel))
-                            .cornerRadius(5)
-                            .overlay(
-                                HStack{
-                                    VStack(alignment: .leading){
-                                        Text("Average Energy Level")
-                                            .font(.custom("EBGaramond-Regular", size: 15))
-                                            .padding(.top, 5)
-                                        Text("\(avgEnergyLevel, specifier: "%.1f")")
-                                            .font(.custom("EBGaramond-Regular", size: 30))
+                    Rectangle()
+                        .cornerRadius(5)
+                        .offset(x: 4, y: 4)
+                        .frame(height: 100)
+                        .overlay(
+                            Rectangle()
+                                .stroke(.black, lineWidth: 5)
+                                .background(returnBoxColor(params: avgSleepLevel))
+                                .cornerRadius(5)
+                                .overlay(
+                                    HStack{
+                                        VStack(alignment: .leading){
+                                            Text("Average Sleep Level")
+                                                .font(.custom("EBGaramond-Regular", size: 15))
+                                                .padding(.top, 5)
+                                            Text("\(avgSleepLevel, specifier: "%.1f")")
+                                                .font(.custom("EBGaramond-Regular", size: 30))
+                                            Spacer()
+                                        }
                                         Spacer()
                                     }
-                                    Spacer()
-                                }
-                                    .padding()
-                                
-                                
-                                
-                            )
-                    )
-                Rectangle()
-                    .cornerRadius(5)
-                    .offset(x: 4, y: 4)
-                    .frame(height: 100)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.black, lineWidth: 5)
-                            .background(returnBoxColor(params: avgSleepLevel))
-                            .cornerRadius(5)
-                            .overlay(
-                                HStack{
-                                    VStack(alignment: .leading){
-                                        Text("Average Sleep Level")
-                                            .font(.custom("EBGaramond-Regular", size: 15))
-                                            .padding(.top, 5)
-                                        Text("\(avgSleepLevel, specifier: "%.1f")")
-                                            .font(.custom("EBGaramond-Regular", size: 30))
-                                        Spacer()
-                                    }
-                                    Spacer()
-                                }
-                                    .padding()
-                                
-                                
-                            )
-                    )
-                
+                                        .padding()
+                                    
+                                    
+                                )
+                        )
+                    
+                }
             }
             
             Spacer()
         }
         .onAppear{
+            isTimeout = false
+            let timeoutDuration: TimeInterval = 10
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeoutDuration) {
+                // Timeout reached, show error message
+                if isLoading {
+                    isTimeout = true
+                    isLoading = true
+                }
+            }
+            
             getAverageEnergySleep(){ rating, energy, sleep in
                 averageRating = rating
                 avgEnergyLevel = energy
                 avgSleepLevel = sleep
-                
-                print("energy: \(energy)")
-                print("rating: \(rating)")
-                print("sleep: \(sleep)")
+                getAverageMood(){ result in
+                    moodRange = result
+                    isLoading = false
+                }
             }
         }
     }
