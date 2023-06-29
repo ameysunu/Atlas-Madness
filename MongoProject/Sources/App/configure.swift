c4fdb15e1d94bbf7b2cb513cbf9c0d88e5a505ba
ameysunu <ameysunu00719@gmail.com>
2023-06-29 21:38:24 +0100

    build: secrets added for mongodb connection

--- MongoProject/Sources/App/configure.swift
+++ MongoProject/Sources/App/configure.swift
@@ -8,7 +8,8 @@ public func configure(_ app: Application) throws {
     
     // Configure the app to connect to a MongoDB deployment. If a connection string is provided via the `MONGODB_URI`
     // environment variable it will be used; otherwise, use the default connection string for a local MongoDB server.
-    try app.mongoDB.configure(Environment.get("MONGODB_URI") ?? "mongodb://35.228.18.140:27017")
+    let secrets = Secrets()
+    try app.mongoDB.configure(Environment.get("MONGODB_URI") ?? "mongodb://\(secrets.mongouserid):\(secrets.mongopassword)@35.228.18.140:27017")
 
     // Use `ExtendedJSONEncoder` and `ExtendedJSONDecoder` for encoding/decoding `Content`. We use extended JSON both
     // here and on the frontend to ensure all MongoDB type information is correctly preserved.
