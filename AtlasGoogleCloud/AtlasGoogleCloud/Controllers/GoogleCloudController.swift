6f50cec7241f98aa222d4c0d5d7c27d9a3a436a2
ameysunu <ameysunu00719@gmail.com>
2023-07-08 18:51:05 +0100

    build: send message to GC added

--- AtlasGoogleCloud/AtlasGoogleCloud/Controllers/GoogleCloudController.swift
+++ AtlasGoogleCloud/AtlasGoogleCloud/Controllers/GoogleCloudController.swift
@@ -494,3 +494,44 @@ func getCurrentGroupChats(groupId: String, completion: @escaping (String) -> Voi
         
         task.resume()
 }
+
+func sendMessageToGroup(groupId: String, userId: String, username: String, messageText: String, completion: @escaping (String?, Error?) -> Void) {
+    guard let url = URL(string: "http://localhost:8080/sendMessage/\(groupId)/\(userId)/\(username)/\(messageText)") else {
+        completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+        return
+    }
+    
+    let messageData: [String: Any] = [
+        "messageId": UUID().uuidString,
+        "sender": [
+            "userId": userId,
+            "username": username
+        ],
+        "content": messageText,
+        "timestamp": Date().ISO8601Format()
+    ]
+    
+    guard let jsonData = try? JSONSerialization.data(withJSONObject: messageData) else {
+        completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to serialize JSON"]))
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = jsonData
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            completion(nil, error)
+            return
+        }
+        
+        if let httpResponse = response as? HTTPURLResponse {
+            let statusCode = httpResponse.statusCode
+            completion("\(statusCode)", nil)
+            print("Response status code: \(statusCode)")
+            // Additional handling of the response if needed
+        }
+    }.resume()
+}
--- AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/GroupChatView.swift
+++ AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/GroupChatView.swift
@@ -13,7 +13,16 @@ struct GroupChatView: View {
     @State private var messages: [ChatMessage] = []
     @State private var userInput = ""
     @State private var userId = getLoginToken()
-    
+
+    func sendMessage() {
+        let userMessage = ChatMessage(text: userInput, isUser: true, isLoading: false)
+        messages.append(userMessage)
+
+        sendMessageToGroup(groupId: groupId, userId: userId!, username: userId!, messageText: userInput) { result, error in
+            print(result)
+        }
+    }
+
     var body: some View {
         VStack {
             Text("Chat")
@@ -22,23 +31,26 @@ struct GroupChatView: View {
                 ScrollView {
                     LazyVStack(alignment: .leading, spacing: 16) {
                         ForEach(messages) { message in
-                            HStack{
-                                Text("\(message.userId!): ")
-                                    .font(.custom("EBGaramond-Regular", size: 20))
+                            HStack {
+                                if let userId = message.userId {
+                                    Text("\(userId): ")
+                                        .font(.custom("EBGaramond-Regular", size: 20))
+                                }
                                 ChatBubble(message: message)
                             }
                         }
                     }
                 }
                 .padding()
-                
+
                 HStack {
                     TextField("Type a message...", text: $userInput)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                    Button(action:{
-                        
-                    }){
+
+                    Button(action: {
+                        sendMessage()
+                        userInput = "" // Clear the text input after sending
+                    }) {
                         Image(systemName: "paperplane.fill")
                             .foregroundColor(.blue)
                     }
@@ -46,33 +58,31 @@ struct GroupChatView: View {
                 .padding()
             }
         }
-        .onAppear{
+        .onAppear {
             getCurrentGroupChats(groupId: groupId) { result in
                 guard let data = result.data(using: .utf8) else {
                     return
                 }
-                
+
                 do {
                     let decodedData = try JSONDecoder().decode(GroupChat.self, from: data)
                     let messageData = decodedData.messages
-                    
+
                     messages = messageData.map { message in
                         let text = message.content
                         let isUser = message.sender.userId == userId
                         let userId = message.sender.username
-                        
+
                         return ChatMessage(text: text, isUser: isUser, isLoading: false, userId: userId)
-                        
-                        
                     }
                 } catch {
                     print("Error decoding chat messages: \(error)")
                 }
             }
-            
         }
     }
 }
+
 
 struct GroupChatView_Previews: PreviewProvider {
     static var previews: some View {
