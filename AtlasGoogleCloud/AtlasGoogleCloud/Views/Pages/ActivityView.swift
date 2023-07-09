97d36b299687571e1d79be1e0d011a5db0fbb25d
ameysunu <ameysunu00719@gmail.com>
2023-07-09 12:34:54 +0100

    build: add activity sheet added

--- AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/ActivityView.swift
+++ AtlasGoogleCloud/AtlasGoogleCloud/Views/Pages/ActivityView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ActivityView: View {
     @State var groupId: String
     @State private var activities: [ActivityData] = []
+    @State var addActivity: Bool = false
 
     var body: some View {
         VStack{
@@ -18,7 +19,9 @@ struct ActivityView: View {
                     .font(.custom("EBGaramond-Regular", size: 35))
                     .padding()
                 Spacer()
-                Button(action:{}){
+                Button(action:{
+                    addActivity = true
+                }){
                     Image(systemName: "plus")
                 }
             }
@@ -66,7 +69,59 @@ struct ActivityView: View {
                 }
             }
         }
+        .sheet(isPresented: $addActivity) {
+            CreateActivity()
+        }
     }
 }
 
 
+struct CreateActivity: View {
+    
+    var userId: String = getLoginToken()!
+    @State var name: String = ""
+    @State var description: String = ""
+    
+    var body: some View {
+        ScrollView{
+            VStack(alignment: .leading) {
+                
+                Text("Add an activity")
+                    .font(.custom("EBGaramond-Regular", size: 35))
+                    .padding(.bottom, 10)
+                
+                TextField("Name",text: $name)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 5.0)
+                        .stroke(lineWidth: 1.0))
+                    .padding(.bottom, 10)
+                    .font(.custom("EBGaramond-Regular", size: 20))
+                
+                Text("Description")
+                    .font(.custom("EBGaramond-Regular", size: 20))
+                
+                TextEditor(text: $description)
+                    .overlay(RoundedRectangle(cornerRadius: 5.0)
+                        .stroke(lineWidth: 1.0))
+                    .frame(height: 200)
+                    .background(Color(.systemGray6))
+                    .padding(.bottom, 10)
+                Spacer()
+                Button(action:{}){
+                    Text("Add")
+                        .font(.custom("EBGaramond-Regular", size: 20))
+                        .padding(8)
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 0.1)
+                            .shadow(color: .black, radius: 10.0))
+                        .background(RoundedRectangle(cornerRadius: 10).fill(.black))
+                        .padding(.top, 20)
+                }
+            }
+        }
+        .padding()
+    }
+}
